b6f1166b3d7c3897e7c37c8c7f411e68cecb6a81
Rodrigo <rodrigomdomingos@gmail.com>
2018-12-06 01:37:03 -0200

    06/12 commit

--- src/main/java/br/com/challenge/controller/TaskController.java
+++ src/main/java/br/com/challenge/controller/TaskController.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
+import com.google.gson.*;
 import br.com.challenge.entity.Task;
 
 import java.util.List;
@@ -15,6 +17,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
+import javax.swing.text.html.ListView;
   
 //Spring MVC
 @Controller
@@ -30,23 +33,28 @@ public class TaskController {
 	  Task task = new Task();
 	  List<Task> list;	
 
-		
+	  /*
+	   * Constructor
+	   */
 	  public TaskController() {
 		  emf = Persistence.createEntityManagerFactory("task");
 		  em = emf.createEntityManager();
 		  getListOfTask();
 	  }
 	  
-	  @RequestMapping("/salvar")
+	  /*
+	   * Save task and update list
+	   */
 	  public void salvar(Task task) {
 		  em.getTransaction().begin();
 		  em.merge(task);
 		  em.getTransaction().commit();
-		  
 		  getListOfTask();
 	  }
 	  
-	  @RequestMapping("/remover")
+	  /*
+	   * remove task and update
+	   */
 	  public void remover(Task task) {
 		  em.getTransaction().begin();
 		  em.remove(task);
@@ -54,7 +62,9 @@ public class TaskController {
 		  
 		  getListOfTask();
 	  }
-	  
+	  /*
+	   * update task 
+	   */
 	  @RequestMapping("/update")
 	  public void update(int id, String task) {
 		  em.getTransaction().begin();
@@ -63,22 +73,46 @@ public class TaskController {
 		  em.getTransaction().commit();
 	  }
 	  
+	  /*
+	   * Get list of all tasks
+	   */
 	  public void getListOfTask() {
 		  Query q = em.createNativeQuery("select * from task", Task.class);
 		  @SuppressWarnings("unchecked")
 		  List<Task> list = (List<Task>) q.getResultList();
 		  this.list = list;
 	  }
-	  		  
-	  	//@RequestMapping("/hello")
+	  	
+	  	/*
+	  	 * Spring mvc add task via url
+	  	 */
+	  	@RequestMapping("/adicionar")
 		public ModelAndView showMessage(
-		@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		@RequestParam(value = "titulo", required = false, defaultValue = "") String titulo) {
+	  		Task nova = new Task();
+			nova.setTitulo(titulo);
+			salvar(nova);
 			ModelAndView mv = new ModelAndView("helloworld");
 			mv.addObject("message", message);
-			mv.addObject("name", name);
 			
 			return mv;
 	  	}
+	  	
+	  	/*
+	  	 * Spring MVC return JSON of all tasks;
+	  	 */
+		@RequestMapping("/getListJSON")
+		public ModelAndView getLisJSON() {
+			getListOfTask();
+			ModelAndView mv = new ModelAndView(	);
+			mv.setView(new MappingJackson2JsonView());
+			mv.addObject(getList());
+			
+			getListOfTask();
+			
+			return mv;
+	  	}
+		
 		
 		public void addTask() {
 			salvar(task);
--- src/test/java/br/com/challenge/ChallengeApplicationTests.java
+++ src/test/java/br/com/challenge/ChallengeApplicationTests.java
@@ -1,16 +1,24 @@
 package br.com.challenge;
 
 import org.junit.Test;
+
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import br.com.challenge.entity.Task;
+import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ChallengeApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void testTask() {
+		String s1 = "Fist task of the day";
+		Task t1 = new Task();
+		t1.setTitulo(s1);
+		
+		assertEquals(t1.getTitulo(), s1);
 	}
 
 }
